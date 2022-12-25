e146ed501f3a055a49e386821bccbc9db7b27b68
Mikhail Kozlov <62540571+Mishaizlesa@users.noreply.github.com>
2022-12-25 20:43:13 +0300

    Update solve_expr.h

--- include/solve_expr.h
+++ include/solve_expr.h
@@ -5,15 +5,24 @@
 #include "queue.h"
 #include "stack.h"
 #include "expression_check.h"
-std::pair<double,bool> solve(std::string str){
+std::pair<double,bool> solve(std::string str, const std::vector<double>& test){
     queue<State>inf;
     queue<State>post;
     std::map<std::string,double>var;
     std::pair<double, bool>res;
     if (!check_str(str,inf,var)) return {0,1};
-    for(auto& el: var){
-        std::cout<<el.first<<" = ";
-        std::cin>>el.second;
+    if (test.size()){
+        int i=0;
+        for(auto& el: var){
+            el.second=test[i];
+            i++;
+        }
+    }
+    else{
+        for(auto& el: var){
+            std::cout<<el.first<<" = ";
+            std::cin>>el.second;
+        }
     }
     if (!check_infix(inf,post)){
         return {0,1};
