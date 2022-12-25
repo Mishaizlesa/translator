30f48f8556645f8b5199ec9875fe5b4de19dcf3a
Mikhail Kozlov <62540571+Mishaizlesa@users.noreply.github.com>
2022-12-25 20:43:59 +0300

    Update main.cpp

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,12 +1,13 @@
 #include "solve_expr.h"
 using namespace std;
 int main() {
+    vector<double>a;
     cout<<"terms of use:\n1 enter all operators and numbers separately by space (it can be more than 1)\n2 integers and decimals are supported\n3 addition, substraction, multiplication and division are supported\n4 Variables are supported\5 Variables must start with latin letter or with _\n6 Variables can consist latin letters, '_' and numbers\n";
     string res;
     while (true) {
         cout<<"enter the expression:\n";
         getline(cin, res);
-        auto tmp=solve(res);
+        auto tmp=solve(res,a);
         if (tmp.second) cout<<"INCORRECT EXPRESSION\n";
         else cout<<tmp.first<<"\n";
         string ans="1";
@@ -20,3 +21,4 @@ int main() {
         cin.ignore();
     }
 }
+
